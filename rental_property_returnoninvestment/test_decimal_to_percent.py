b495716135e2addc24eb37f23eec307c66ac7551
Nathan Mancz <nathan@mancz.com>
2023-02-04 22:42:31 +0000

    Updates

---
+++ rental_property_returnoninvestment/test_decimal_to_percent.py
@@ -0,0 +1,13 @@
+import unittest
+
+from rental_property_roi import decimal_to_percent
+
+class TestRentalPropertyRoi(unittest.TestCase):
+
+    def test_21_percent(self):
+        self.assertEqual(decimal_to_percent(0.21), 21)
+
+    def test_3_percent(self):
+        self.assertEqual(decimal_to_percent(0.03), 3)
+
+    
