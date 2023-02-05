e4baf70dcaaf47cd2e872ebc4f3ef225e54f5c40
Nathan Mancz <nathan@mancz.com>
2023-02-05 20:13:27 +0000

    Updates

--- take_home_pay_calculator/test_tax_payable.py
+++ take_home_pay_calculator/test_tax_payable.py
@@ -5,10 +5,10 @@ from take_home_pay_calculator import tax_payable
 class TestTaxPayable(unittest.TestCase):
 
     def test_27000(self):
-        self.assertEqual(tax_payable(27000), 2886)
+        self.assertEqual(tax_payable(27000), 2616)
     
     def test_65000(self):
-        self.assertEqual(tax_payable(65000), 13432)
+        self.assertEqual(tax_payable(65000), 12132)
 
     def test_10000(self):
         self.assertEqual(tax_payable(10000), 0)
